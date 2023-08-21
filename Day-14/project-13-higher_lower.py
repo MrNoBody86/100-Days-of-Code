8fbc19aa980c0752d3003a27265e8868168eb942
Aryan Chouksey <ayushchauksey08@gmail.com>
2023-08-21 18:40:13 +0530

    Day-14 minor bug fixes, Day-15 : First commit

--- Day-14/project-13-higher_lower.py
+++ Day-14/project-13-higher_lower.py
@@ -18,52 +18,52 @@ def clear():
     elif sys.platform.startswith('win32'):
         os.system('cls')
 
-# Function to print the choises and ask to choose
-def round(choise_A , choise_B):
+# Function to print the choices and ask to choose
+def round(choice_A , choice_B):
     """UI for the game"""
-    print(f"Compare A : {choise_A['name']}, a {choise_A['description']}, from {choise_A['country']}\n")
+    print(f"Compare A : {choice_A['name']}, a {choice_A['description']}, from {choice_A['country']}\n")
     print(vs)
-    print(f"\nAgainst B : {choise_B['name']}, a {choise_B['description']}, from {choise_B['country']}")
+    print(f"\nAgainst B : {choice_B['name']}, a {choice_B['description']}, from {choice_B['country']}")
     return input("Who has more followers? Type 'A' or 'B' : ").lower()
     
 # Function for deciding result of a round
-def round_result(choise_A , choise_B , player_choise):
-    """Returns True for correct choise and visa versa"""
-    A_follwers = int(choise_A['follower_count'])
-    B_follwers = int(choise_B['follower_count'])
-    if A_follwers > B_follwers :
-        if player_choise == 'a':
+def round_result(choice_A , choice_B , player_choice):
+    """Returns True for correct choice and visa versa"""
+    A_followers = int(choice_A['follower_count'])
+    B_followers = int(choice_B['follower_count'])
+    if A_followers > B_followers :
+        if player_choice == 'a':
             return True
         else :
             return False
     else :
-        if player_choise == 'b':
+        if player_choice == 'b':
             return True
         else :
             return False
 
-# Fuction for game
+# Function for game
 def higher_lower():
     """starts the higher-lower game"""
     clear()
     print(logo)
-    # Generate random choises from data for first round
+    # Generate random choices from data for first round
     import random
-    choises = random.sample(data,2)
-    choise_A = choises[0]
-    choise_B = choises[1]
+    choices = random.sample(data,2)
+    choice_A = choices[0]
+    choice_B = choices[1]
 
     answer = True
     score = 0
     while answer:
-        player_choise = round(choise_A,choise_B)
-        answer = round_result(choise_A , choise_B , player_choise)
+        player_choice = round(choice_A,choice_B)
+        answer = round_result(choice_A , choice_B , player_choice)
         if answer :
             score += 1
-            choise_A = choise_B
-            choise_B = random.choice(data)
-            while choise_A == choise_B:
-                choise_B = random.choice(data)
+            choice_A = choice_B
+            choice_B = random.choice(data)
+            while choice_A == choice_B:
+                choice_B = random.choice(data)
             clear()
             print(logo)
             print(f"You're right! Current score: {score}")
