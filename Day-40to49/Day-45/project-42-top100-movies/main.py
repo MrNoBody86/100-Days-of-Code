028e2da504f1f65c8bb5077868a02d365b22f3e1
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-04-07 20:25:34 +0530

    Completed Day 45

--- Day-40to49/Day-45/project-42-top100-movies/main.py
+++ Day-40to49/Day-45/project-42-top100-movies/main.py
@@ -1,10 +1,20 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-response = requests.get(url = URL,timeout=None)
+# Write your code below this line 👇
 
-print(response.text)
+response = requests.get(URL,timeout=None)
+website_html = response.text
 
+soup = BeautifulSoup(website_html, "html.parser")
 
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("Day-40to49/Day-45/project-42-top100-movies/movies.txt", mode="w",encoding="utf-8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
